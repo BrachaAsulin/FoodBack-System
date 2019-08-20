63fbc03cc34c1e974aa6ab839ec8a843c65f8640
noam bezaleli <noamm80@gmail.com>
2019-08-20 20:14:08 +0300

    עדכוניםםםםםם

--- MonitoringLifestyle/Views/GoalsUserControl.xaml.cs
+++ MonitoringLifestyle/Views/GoalsUserControl.xaml.cs
@@ -24,5 +24,10 @@ namespace MonitoringLifestyle.Views
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
